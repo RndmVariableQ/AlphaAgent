72c94f197e1231af6df671b496d8a1af7197073e
RndmVariableQ <ziyitang886@gmail.com>
2025-03-03 15:59:12 +0800

    v0.1

---
+++ alphaagent/app/qlib_rd_loop/factor_mining.py
@@ -0,0 +1,35 @@
+"""
+Factor workflow with session control
+"""
+
+from typing import Any
+
+import fire
+
+from alphaagent.app.qlib_rd_loop.conf import ALPHA_AGENT_FACTOR_PROP_SETTING
+from alphaagent.components.workflow.alphaagent_loop import AlphaAgentLoop
+from alphaagent.core.exception import FactorEmptyError
+from alphaagent.log import logger
+from alphaagent.log.time import measure_time
+
+
+def main(path=None, step_n=None, direction=None):
+    """
+    Autonomous alpha factor mining. 
+
+    You can continue running session by
+
+    .. code-block:: python
+
+        dotenv run -- python rdagent/app/qlib_rd_loop/factor_alphaagent.py $LOG_PATH/__session__/1/0_propose  --step_n 1  --potential_direction "[Initial Direction (Optional)]"  # `step_n` is a optional paramter
+
+    """
+    if path is None:
+        model_loop = AlphaAgentLoop(ALPHA_AGENT_FACTOR_PROP_SETTING, direction)
+    else:
+        model_loop = AlphaAgentLoop.load(path)
+    model_loop.run(step_n=step_n)
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
